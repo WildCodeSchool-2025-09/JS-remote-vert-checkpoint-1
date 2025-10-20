e58d0b5302cb2ca6336375b51abc777127b41b34
Lisa Bureau <bureau.lisa03@gmail.com>
2025-10-20 11:08:00 +0200

    finish JS step 2 in part 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -5,3 +5,14 @@ const avatar = document.querySelector(".avatar");
 avatar.addEventListener("click", function () {
      avatar.src = "image/avatar.svg";
 });
+
+// Step 2 part 2
+
+const button = document.querySelector("button");
+const firstName = document.querySelector("#firstname");
+
+button.addEventListener("click", () => {
+    newName = prompt("Enter your name :");
+    firstName.innerHTML = newName;
+    firstName.style.color = "var(--light)";
+});
