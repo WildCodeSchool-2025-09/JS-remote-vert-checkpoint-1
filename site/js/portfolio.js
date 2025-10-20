f9882ce268b0958071bab0ced56005fe1cc7f0c1
Florent Goujon <gouflo@gmail.com>
2025-10-20 12:20:29 +0200

    JS step 3

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -8,7 +8,13 @@ const firstname = document.getElementById("firstname")
 
 const button = document.getElementById("buttonModification")
 
+const backgroundBanner = document.querySelectorAll(".description.pink-bg")
+
+console.log(backgroundBanner)
+
 button.addEventListener("click", () => {
     firstname.style.color = "white";
-    firstname.textContent= prompt("Renseignez votre nom")
+    firstname.textContent= prompt("Renseignez votre nom");
+    const color = prompt("Choisissez une couleur")
+    backgroundBanner.forEach(banner => {banner.style.backgroundColor = color;})
 })
