b18d63f345cd26bab62443d36e7880492a14ed4d
Mickaël Lambert <mickael.lambert@wildcodeschool.com>
2025-10-20 14:25:57 +0200

    Finish algo 1

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -9,7 +9,6 @@ thirdAngle(20, 80) doit retourner 80
 function thirdAngle(a, b) {
   return 180 - (a + b);
 }
-console.log(thirdAngle(90, 30));
-console.log(thirdAngle(20, 80));
+
 
 module.exports = thirdAngle;
