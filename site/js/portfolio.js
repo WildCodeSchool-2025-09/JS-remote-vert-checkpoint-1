704c234527f988d675b4721f1846e01d8f9e6ea8
LUNA SOLER <soler.luna@outlook.fr>
2025-10-20 11:52:41 +0200

    Step 3 of DOM done, I'm proud..

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -6,11 +6,14 @@ myAvatar.onclick=function() {
 
 const modifyText = document.querySelector('.my-button');
 const userName = document.getElementById('firstname');
+const bannerColor = document.querySelector('.pink-bg');
 
 modifyText.onclick=function() {
     const newName = prompt("What's your name ?")
-    if (newName) {
+    const newColor = prompt("Enter a color ?")
+    if (newName && newColor) {
         userName.innerHTML = newName;
         userName.style.color = "white";
+        bannerColor.style.backgroundColor = newColor;
     }
 };
