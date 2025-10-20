5c5def62e7123a80d89fcfd6b0c038a0ea2c9e89
FIRST_NAME LAST_NAME <Pro.emeric.m@gmail.com>
2025-10-20 10:23:37 +0200

    finish algo2 in part1

--- algo/2/isLeapYear.js
+++ algo/2/isLeapYear.js
@@ -5,10 +5,33 @@ Rappel : Une année bissextile vérifie **une** de ces règles :
 - Elle est divisible par 400
 Ex : 2004, 2016 et 2020 sont des années bissextiles
 Rappel : Pour vérifier si un nombre est divisible par un autre, tu peux utiliser l'opérateur "modulo" (%)
+
+
+si 1 année bisextile si oui true si non false 
+divisible par 4 true et non divisible par 100 false 
+ 2004 / 4 = 501 4* 501 = 2004 donc modulo  0  || 2005/4 = 501.25 nombre pas entier donc faux 
+ 2004 / 100 = 20.04 nombre non entier donc faux || 2005/100 = 20.05 nombre non entier donc faux 
+divisible par 400 true
+
+
+test sur 2004 / 2016 / 2020 utilisier modulo 7%3 = 1 4%2=0
+
+ }else{}
+           console.log (false);
+            if (year % 100 != 0){
+            console.log(true);
+           }
+
 */
-
 function isLeapYear(year) {
-  // Your code here !
+         if (year % 4 === 0 && year % 100 != 0){
+          return(true);
+         }
+         else if ( year % 400 === 0){
+         return (true);
+         }else{
+          return (false);
+         }
 }
-
+     
 module.exports = isLeapYear;
