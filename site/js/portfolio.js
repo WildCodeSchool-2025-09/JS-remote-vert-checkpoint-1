6bf0c325518442d9dce09ffb5cbf113a62501596
Lollye-dev <lorenvisset@gmail.com>
2025-10-20 12:25:58 +0200

    finish JS Step2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -4,3 +4,12 @@ avatar.onclick = function () {
     avatar.src="image/avatar-bis.png";
     avatar.alt="new avatar";
 }
+
+const button = document.querySelector('.button');
+const firstname = document.querySelector('#firstname'); 
+
+button.addEventListener("click", function(event) {
+    const newName = prompt("Enter your name :");   
+    firstname.innerHTML = newName;
+    firstname.style.color = "white";
+})
