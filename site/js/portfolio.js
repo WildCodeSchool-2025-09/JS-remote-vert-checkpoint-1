29a149777b22ed7b1d8613db4762a6c26d24f280
Kevin Ressegaire <176156975+earzalien@users.noreply.github.com>
2025-10-20 11:55:08 +0200

    finish JS step 2 of part 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -3,3 +3,13 @@ imageAvatar.addEventListener('click', () => {
     imageAvatar.src = "image/avatar.svg";
 
 });
+
+function changename() {
+    let button = document.getElementById('namebutton');
+    button.addEventListener('click', () => {
+        let newname = prompt('What\'s your name ?')
+        replacename = document.getElementById('firstname');
+        replacename.textContent = newname;
+        replacename.style.color = 'white'
+    });
+}
