c050c301e86d292a3e6b0923c8c642c8b7be0b56
FIRST_NAME LAST_NAME <julpaultes@gmail.com>
2025-10-20 12:54:17 +0200

    fin des bonus portfolio, bon appetit

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -10,6 +10,7 @@ let description =document.querySelectorAll(".description.pink-bg")
 let btnTextColor = document.querySelector(".modify-color-and-text")
 let firstname = document.querySelector("#firstname")
 let pinkText = document.querySelectorAll(".pink-text")
+let links = document.querySelectorAll("a")
 
 
 btnTextColor.addEventListener("click", function () {
@@ -19,11 +20,39 @@ btnTextColor.addEventListener("click", function () {
         description.forEach(el => el.style.backgroundColor = newColor)
         btnTextColor.style.backgroundColor= newColor
         pinkText.forEach(el => el.style.color= newColor)
+        links.forEach(el => el.style.color= newColor)
       }
-
       if (newName && newName.trim() !== ""){
         firstname.textContent = newName
         firstname.style.color = "white"
       }
   }
 )
+
+
+let modifyTools = document.querySelector(".modify-tools")
+let devTools = document.querySelector("#front-dev-tools")
+const myDevTools = ["VSCode", "GitHub", "Terminal"]
+
+modifyTools.addEventListener("click", function() {
+  devTools.innerHTML = ""
+  for (let i = 0; i < myDevTools.length; i++)
+  { let li = document.createElement("li") 
+  li.textContent = myDevTools[i]
+  devTools.appendChild(li)
+  }
+})
+
+const form = document.querySelector("#form");
+let addInput = document.querySelector("#new-input")
+let skill = document.querySelector("#dev-skills")
+
+form.onsubmit = function (event){
+event.preventDefault()
+const newSkill = document.createElement("li")
+newSkill.innerHTML = addInput.value;
+skill.appendChild(newSkill);
+addInput.value = ""
+
+}
+  
