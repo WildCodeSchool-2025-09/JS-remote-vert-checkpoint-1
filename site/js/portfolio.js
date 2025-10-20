b0dc9a53ea92dba9eb03907749a2b5fa226501bd
Kevin Ressegaire <176156975+earzalien@users.noreply.github.com>
2025-10-20 12:10:54 +0200

    finish JS step 3 of part 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -8,8 +8,13 @@ function changename() {
     let button = document.getElementById('namebutton');
     button.addEventListener('click', () => {
         let newname = prompt('What\'s your name ?')
+        let newcolor = prompt('Enter a color :\nby exemple #750ff7 ')
         replacename = document.getElementById('firstname');
         replacename.textContent = newname;
-        replacename.style.color = 'white'
+        replacename.style.color = 'white';
+        replacecolor = document.getElementsByClassName('pink-bg');
+        for (let i = 0; i < replacecolor.length; i++) {
+            replacecolor[i].style.backgroundColor = newcolor;
+        }
     });
 }
