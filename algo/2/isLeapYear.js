23389efa61bd940e03b940a0c9322063252c254d
Mickaël Lambert <mickael.lambert@wildcodeschool.com>
2025-10-20 14:43:55 +0200

    Finish algo 2

--- algo/2/isLeapYear.js
+++ algo/2/isLeapYear.js
@@ -9,20 +9,7 @@ Rappel : Pour vérifier si un nombre est divisible par un autre, tu peux utilise
 */
 
 function isLeapYear(year) {
-  if (year % 4 === 0 && year % 100 !== 0) {
-    return true;
-  }
-  if (year % 400 === 0) {
-    return true;
-  } else {
-    return false;
-  }
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
 }
-
-console.log(isLeapYear(1900));
-console.log(isLeapYear(2000));
-console.log(isLeapYear(2004));
-console.log(isLeapYear(2019));
-console.log(isLeapYear(2020));
 
 module.exports = isLeapYear;
