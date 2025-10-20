9984848b0987334119656997adc74c21b392e105
LUNA SOLER <soler.luna@outlook.fr>
2025-10-20 11:43:29 +0200

    Did the 2nd step of DOM. It was long, my brain is on fire and idk if it's the good way but it works git add .!

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -2,3 +2,15 @@ const myAvatar = document.querySelector('.my-avatar')
 myAvatar.onclick=function() {
     myAvatar.src="./image/avatar.svg"
 };
+
+
+const modifyText = document.querySelector('.my-button');
+const userName = document.getElementById('firstname');
+
+modifyText.onclick=function() {
+    const newName = prompt("What's your name ?")
+    if (newName) {
+        userName.innerHTML = newName;
+        userName.style.color = "white";
+    }
+};
