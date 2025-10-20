81c63291d00dc6423453d9a665e7e4117fd4e978
Colombine972 <decidela972@me.com>
2025-10-20 13:59:05 +0200

    JS step 3

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -9,9 +9,13 @@ avatar.src ="image/avatar.svg";
 /*change le nom dans la description*/
 const changeNameBtn = document.getElementById("changeNameBtn");
 const firstName = document.getElementById("firstName");
+const section = document.getElementById("section");
 
 changeNameBtn.addEventListener("click", () =>{
-    let userName = prompt("What's your name ?");
-    firstName.textContent =userName;
-})
+    const userName = prompt("Enter your name :");
+     firstName.textContent =userName;
+
+    const newColor = prompt("Enter a background color:", "#750ff7");
+    section.style.setProperty("--lightWildColor") = newColor; 
+});
 
