0b23de04086b4062e2c988048956130c0a9a120c
ClarissaCardarelli <ccardare@gmail.com>
2025-10-20 11:31:01 +0200

    finished JS steps 3 & 4

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -4,8 +4,14 @@ avatar.addEventListener("click", () => (avatar.src = "image/avatar.svg"));
 
 const nameBtn = document.querySelector("#name-btn");
 const firstName = document.querySelector("#firstname");
+const coloredBgs = document.querySelectorAll(".pink-bg");
+const coloredTexts = document.querySelectorAll(".pink-text");
 
 nameBtn.addEventListener("click", () => {
+  const newBgColor = prompt("Enter a color:");
+  coloredBgs.forEach((element) => (element.style.backgroundColor = newBgColor));
+  coloredTexts.forEach((element) => (element.style.color = newBgColor));
+
   const name = prompt("Enter your name:");
   firstName.textContent = name;
   firstName.style.color = "white";
