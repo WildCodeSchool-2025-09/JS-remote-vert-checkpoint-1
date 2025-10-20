42161d67425bbe8668a8a103129244cde9608315
Colombine972 <decidela972@me.com>
2025-10-20 13:03:04 +0200

    finish JS step 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,6 +1,17 @@
 /* selectionne ton image*/
 const avatar = document.getElementById("avatar");
 
-avatar.addEventListener('click', () => {
+avatar.addEventListener("click", () => {
 avatar.src ="image/avatar.svg";
 })
+
+
+/*change le nom dans la description*/
+const changeNameBtn = document.getElementById("changeNameBtn");
+const firstName = document.getElementById("firstName");
+
+changeNameBtn.addEventListener("click", () =>{
+    let userName = prompt("What's your name ?");
+    firstName.textContent =userName;
+})
+
