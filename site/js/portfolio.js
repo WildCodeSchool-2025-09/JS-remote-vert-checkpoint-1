502eea5f86a411ba92e69a9f5686ca593031149a
Lisa Bureau <bureau.lisa03@gmail.com>
2025-10-20 11:26:04 +0200

    finish JS step 4 in part 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -6,16 +6,25 @@ avatar.addEventListener("click", function () {
      avatar.src = "image/avatar.svg";
 });
 
-// Step 2 part 2
+// Step 2, 3, 4 part 2
 
 const button = document.querySelector("button");
 const firstName = document.querySelector("#firstname");
-const background = document.querySelector(".pink-bg");
+const background = document.querySelectorAll(".pink-bg");
+const textPink = document.querySelectorAll(".pink-text");
 
 button.addEventListener("click", () => {
+    newColor = prompt("Enter a color :");
     newName = prompt("Enter your name :");
     firstName.innerHTML = newName;
     firstName.style.color = "var(--light)";
-    background.style.backgroundColor = "#750ff7";
-    button.style.backgroundColor = "#750ff7";
+    button.style.backgroundColor = newColor;
+
+    background.forEach((element) => {
+        element.style.backgroundColor = newColor;
+    })
+
+    textPink.forEach((element) => {
+        element.style.color = newColor;
+    })
 });
