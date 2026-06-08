f2d18970b252cd6e31fb85a6892e28a05f255efa
czagoh <dorian.pennegat@hotmail.fr>
2026-06-08 11:12:42 +0200

    Step 02 - JS Portfolio - Added Button to modify name

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -4,3 +4,13 @@ avatar.addEventListener('click', function() {
   avatar.src = 'image/avatar.svg';
 });
 
+const firstname = document.querySelector('#firstname');
+const button = document.querySelector('#change-name');
+
+button.addEventListener('click', function() {
+  const name = prompt('Quel est ton prénom ?');
+  if (name) {
+    firstname.textContent = name;
+    firstname.style.color = 'white';
+  }
+});
