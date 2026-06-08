6086165f899ce8c9b02b98ebeb4d95c881824d87
czagoh <dorian.pennegat@hotmail.fr>
2026-06-08 09:35:09 +0200

    01 - ThirdAngle done

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -7,7 +7,10 @@ thirdAngle(20, 80) doit retourner 80
 */
 
 function thirdAngle(a, b) {
-  // Your code here !
+  return 180 - a - b;
 }
+
+console.log(thirdAngle(60, 60));
+console.log(thirdAngle(90, 40));
 
 module.exports = thirdAngle;
