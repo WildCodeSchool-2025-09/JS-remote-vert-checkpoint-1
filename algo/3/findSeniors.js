38d9a5ce2c9fb14ae352cb81a140c7d9763bdd79
Julien Roussel <julienrousselpro13@gmail.com>
2026-06-08 10:57:33 +0200

    fish css step 6

--- algo/3/findSeniors.js
+++ algo/3/findSeniors.js
@@ -24,9 +24,28 @@ const persons = [
 ];
 
 */
+const persons = [
+  { name: 'Mary', experience: 2, job: 'web dev' },
+  { name: 'Tony', experience: 6, job: 'data analyst' },
+  { name: 'John', experience: 2, job: 'data analyst' },
+  { name: 'Jane', experience: 6, job: 'web dev' },
+  { name: 'Maggie', experience: 2, job: 'web dev' },
+  { name: 'Leonardo', experience: 2, job: 'data analyst' },
+  { name: 'Carla', experience: 4, job: 'data analyst' },
+  { name: 'Mickael', experience: 7, job: 'web dev' },
+  { name: 'Penelope', experience: 7, job: 'web dev' },
+  { name: 'Homer', experience: 5, job: 'data analyst' },
+  { name: 'Leonardo', experience: 2, job: 'data analyst' },
+  { name: 'Carla', experience: 4, job: 'web dev' },
+  { name: 'Lisa', experience: 3, job: 'web dev' },
+  { name: 'Millie', experience: 5, job: 'data analyst' },
+  { name: 'Penelope', experience: 7, job: 'web dev' },
+];
 
 function findSeniors(persons) {
-  // Your code here !
+  const seniorDev = persons.filters(persons => persons.job === 'web dev' && persons.experience > =5);
+  const seniorDataAnalyst = persons.filters(persons => persons.job === 'data analyst' && persons.experience >= 5);
+  return [seniorDev, seniorDataAnalyst] 
 }
 
 module.exports = findSeniors;
