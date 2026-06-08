c7e1ac83929f5a8c3b93041def1ae83a2546d856
Yannick LDL <yann.lebas33@gmail.com>
2026-06-08 13:11:53 +0200

    update step 2, still stuck

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,3 +1,20 @@
 const avatar = document.querySelector(".avatar")
 
-avatar.addEventListener("click", () => {avatar.src = "image/avatar-bis.png"})
+avatar.addEventListener("click", () => {avatar.src = "image/avatar-bis.png"});
+
+
+
+
+const bouton = document.querySelector("#bouton");
+
+let name = document.querySelector("#firstname");
+
+
+bouton.addEventListener("click", () => {
+    let newName = prompt("enter your name");
+
+    name.textContent = newName;
+});
+    
+
+
