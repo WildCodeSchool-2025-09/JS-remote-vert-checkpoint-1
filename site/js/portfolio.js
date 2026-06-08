a0102d3add03586b9ee12fef5b2063de1349fff3
THOMAS FABULET <thomas.fabulet@gmail.com>
2026-06-08 11:35:43 +0200

    finish JS step 2 on part 3

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -3,3 +3,12 @@ const activeAvatar = document.getElementById("avatar");
 activeAvatar.addEventListener("click", function(){
    activeAvatar.src = "/site/image/avatar.svg";
 });
+
+const button = document.getElementById("prompt-name");
+const name = document.getElementById("firstname");
+
+button.addEventListener("click", function(){
+    const userName = prompt ('Enter your name:')
+        name.textContent = userName;
+        name.style.color = "white";
+});
