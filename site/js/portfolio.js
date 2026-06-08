56e7cb53d04c361259fce5e17a0cdb25bf4bb7d6
Sophie LDL <sophievigneron33@gmail.com>
2026-06-08 11:46:53 +0200

    finish step2 of JS

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -2,3 +2,11 @@ const avatar = document.getElementById("avatar");
 avatar.addEventListener("click", () => {
     let avatarChange = avatar.setAttribute("src", "image/avatar.svg")
 });
+
+
+const modifButton = document.getElementById("modif-button");
+const firstName = document.getElementById("firstname");
+modifButton.addEventListener("click", () => {
+    let newName = firstName.textContent = prompt("What\'s you name ?");
+    firstName.style.color = "white";
+});
