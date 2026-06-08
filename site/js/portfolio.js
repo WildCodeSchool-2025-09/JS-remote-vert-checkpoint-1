ac0ff086bd1c7b0c1d643cdc0b84df57dadd87a6
Laurent Koehler <laurent.koehler@gmail.com>
2026-06-08 12:15:01 +0200

    JS step 3 done

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -2,3 +2,21 @@
 function changeImage() {
     document.querySelector('.avatar').src = "./image/avatar-bis.png";
 }
+
+function changeName() {
+
+    const newColor = prompt("enter a color", '#750ff7');
+    if (newColor) {
+        document.querySelector(".pink-bg").style.backgroundColor = newColor
+    }
+
+
+    const newName = prompt("Enter you name:");
+    if (newName) {
+        const span = document.querySelector("#firstname");
+        span.textContent = newName;
+        span.style.color = 'white';
+    }
+
+
+}
