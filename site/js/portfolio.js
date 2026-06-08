faf333253b18ba1245f16277f1fea7015b53bd4a
THOMAS FABULET <thomas.fabulet@gmail.com>
2026-06-08 12:33:23 +0200

    finish JS step 4 in part 3

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -6,11 +6,18 @@ activeAvatar.addEventListener("click", function(){
 
 const button = document.getElementById("prompt-name");
 const name = document.getElementById("firstname");
-const colorBG = document.getElementById('pinkBG');
+const colorBg = document.querySelectorAll("#pinkBG");
+const pinkText = document.querySelectorAll(".pink-text");
 
 button.addEventListener("click", function(){
     const newColor = prompt ('choose your color:')
-        colorBG.style.backgroundColor = newColor;
+     for (let j = 0; j < pinkText.length; j++){
+        pinkText[j].style.color = newColor;
+    }
+    for (let i = 0; i < colorBg.length; i++){
+        colorBg[i].style.backgroundColor = newColor;
+     }
+     
     const userName = prompt ('Enter your name:')
         name.textContent = userName;
         name.style.color = "white";
