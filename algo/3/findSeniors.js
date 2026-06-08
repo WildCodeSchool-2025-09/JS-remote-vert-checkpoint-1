593cb82c51818426b11c3700a940df09b120a128
Frederic Briand <fbr91144@gmail.com>
2026-06-08 09:51:46 +0200

    finish algo3

--- algo/3/findSeniors.js
+++ algo/3/findSeniors.js
@@ -25,8 +25,37 @@ const persons = [
 
 */
 
+const persons = [
+  { name: 'Mary', experience: 2, job: 'web dev' },
+  { name: 'Tony', experience: 6, job: 'data analyst' },
+  { name: 'John', experience: 2, job: 'data analyst' },
+  { name: 'Jane', experience: 6, job: 'web dev' },
+  { name: 'Maggie', experience: 2, job: 'web dev' },
+  { name: 'Leonardo', experience: 2, job: 'data analyst' },
+  { name: 'Carla', experience: 4, job: 'data analyst' },
+  { name: 'Mickael', experience: 7, job: 'web dev' },
+  { name: 'Penelope', experience: 7, job: 'web dev' },
+  { name: 'Homer', experience: 5, job: 'data analyst' },
+  { name: 'Leonardo', experience: 2, job: 'data analyst' },
+  { name: 'Carla', experience: 4, job: 'web dev' },
+  { name: 'Lisa', experience: 3, job: 'web dev' },
+  { name: 'Millie', experience: 5, job: 'data analyst' },
+  { name: 'Penelope', experience: 7, job: 'web dev' },
+];
+
 function findSeniors(persons) {
-  // Your code here !
+  const devsWebSenior = [];
+  const dataAnalystSenior = [];
+
+  persons.forEach(element => {
+      if (element.experience >= 5 && element.job === "web dev") {
+      devsWebSenior.push(element);
+    }
+    if (element.experience >= 5 && element.job === "data analyst") {
+        dataAnalystSenior.push(element);
+    }
+  });
+  return [devsWebSenior, dataAnalystSenior]
 }
 
 module.exports = findSeniors;
