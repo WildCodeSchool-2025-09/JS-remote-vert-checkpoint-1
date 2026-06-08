db367749a209fccf0a7c23dbdd4d1354638b7e3a
czagoh <dorian.pennegat@hotmail.fr>
2026-06-08 11:21:11 +0200

    Step 03 - JS Portfolio - Added option to the button to change the bg color

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -8,9 +8,13 @@ const firstname = document.querySelector('#firstname');
 const button = document.querySelector('#change-name');
 
 button.addEventListener('click', function() {
-  const name = prompt('Quel est ton prénom ?');
+  const name = prompt('Quel est ton prénom ?'); 
   if (name) {
     firstname.textContent = name;
     firstname.style.color = 'white';
+  }
+
+  const color = prompt ('Quelle couleur de background ? (ex: #750ff7)'); {
+    document.querySelector('.pink-bg').style.backgroundColor = color;
   }
 });
