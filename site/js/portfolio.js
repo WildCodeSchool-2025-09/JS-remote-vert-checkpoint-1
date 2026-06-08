7b869f29ba6b0f2eb819e4558d764f42c26ad35d
Sophie LDL <sophievigneron33@gmail.com>
2026-06-08 12:27:05 +0200

    attempt for step4 of JS

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -7,8 +7,11 @@ avatar.addEventListener("click", () => {
 const modifButton = document.getElementById("modif-button");
 const firstName = document.getElementById("firstname");
 const pinkBG = document.getElementById("pink");
+const colors = document.querySelectorAll(".pink-text,.pink-bg")
 modifButton.addEventListener("click", () => {
     let newBG = pinkBG.style.backgroundColor = prompt("Wich color ?");
     let newName = firstName.textContent = prompt("What\'s you name ?");
     firstName.style.color = "white";
+    let newBgColors = colors.style.backgroundColor = newBG;
+    let newTxtColors = colors.style.color = newBG;
 });
